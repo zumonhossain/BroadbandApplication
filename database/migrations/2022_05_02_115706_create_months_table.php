9f10af6cb9e746f65a65740fb28d3e2fe7349b57
zumonhossain <zumon15-13169@diu.edu.bd>
2022-05-02 18:06:30 +0600

    Month Database Design

---
+++ database/migrations/2022_05_02_115706_create_months_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('months', function (Blueprint $table) {
+            $table->bigIncrements('month_id');
+            $table->string('month_name_en', 20);
+            $table->string('month_name_bn', 30);
+            $table->boolean('month_status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('months');
+    }
+};

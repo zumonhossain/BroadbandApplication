20aee3eb8a0dc9a69ece5204a0b4832dbb294ddf
zumonhossain <zumon15-13169@diu.edu.bd>
2022-05-03 22:48:41 +0600

    shipping database create

---
+++ database/migrations/2022_05_02_122119_create_upazilas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('upazilas', function (Blueprint $table) {
+            $table->bigIncrements('upazila_id');
+            $table->integer('division_id');
+            $table->integer('district_id');
+            $table->string('upazila_name',100);
+            $table->boolean('upazila_status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('upazilas');
+    }
+};
---
+++ database/migrations/2022_05_02_122401_create_unions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unions', function (Blueprint $table) {
+            $table->bigIncrements('union_id');
+            $table->integer('division_id');
+            $table->integer('district_id');
+            $table->integer('upazila_id');
+            $table->string('union_name',100);
+            $table->boolean('union_status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unions');
+    }
+};

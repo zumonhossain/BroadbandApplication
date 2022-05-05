77787fe98103953f7cf9807df2dde5ffdf32325b
zumonhossain <zumon15-13169@diu.edu.bd>
2022-05-05 19:47:18 +0600

    Service Type Database Design

---
+++ database/migrations/2022_05_05_134502_create_service_types_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('service_types', function (Blueprint $table) {
+            $table->bigIncrements('service_type_id');
+            $table->string('service_name');
+            $table->boolean('service_status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('service_types');
+    }
+};

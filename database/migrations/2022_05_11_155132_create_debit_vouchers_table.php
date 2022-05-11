725472307c65fb469e822508c9498ca70a680aff
zumonhossain <zumon15-13169@diu.edu.bd>
2022-05-11 21:56:07 +0600

    debit voucher controller & database design

---
+++ database/migrations/2022_05_11_155132_create_debit_vouchers_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('debit_vouchers', function (Blueprint $table) {
+            $table->bigIncrements('debit_voucher_id');
+            $table->unsignedBigInteger('transaction_id');
+            $table->unsignedBigInteger('debit_type_id');
+            $table->date('expense_date');
+            $table->float('amount',11,2);
+            $table->integer('debited_told');
+            $table->integer('credited_from_id');
+            $table->integer('expense_by_id');
+            $table->integer('year');
+            $table->integer('month_id');
+            $table->string('voucher_file_path');
+            $table->unsignedBigInteger('creator');
+            $table->boolean('debit_voucher_status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('debit_vouchers');
+    }
+};
